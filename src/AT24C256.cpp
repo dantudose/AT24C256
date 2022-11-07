dd292e7bf24a0d896551dbcf4320ed29ce46638a
Dan Tudose <dantudose1984@gmail.com>
2022-11-07 21:26:53 +0200

    Update AT24C256.cpp
    Modified code to use any Wire interface.

--- src/AT24C256.cpp
+++ src/AT24C256.cpp
@@ -3,35 +3,36 @@
 #include "Wire.h"
 #include "AT24C256.h"
 
-AT24C256::AT24C256(int address){
-	Wire.begin();
+AT24C256::AT24C256(int address, TwoWire *twi){
+	_twi = twi;
+	_twi->begin();
 	_address = address;
 }
 
-void AT24C256::write(uint16_t writeAddress, uint8_t* data, uint16_t len){
+void AT24C256::write(uint16_t writeAddress, uint8_t* data, uint8_t len){
 
-	Wire.beginTransmission(_address);
-	Wire.write((byte)(writeAddress & 0xFF00) >> 8);
-	Wire.write((byte)(writeAddress & 0x00FF));
+	_twi->beginTransmission(_address);
+	_twi->write((byte)(writeAddress & 0xFF00) >> 8);
+	_twi->write((byte)(writeAddress & 0x00FF));
 	uint8_t i;
 	for(i = 0; i < len; i++){
-		Wire.write(data[i]);
+		_twi->write(data[i]);
 	}
-	Wire.endTransmission();
+	_twi->endTransmission();
 
 }
 
-void AT24C256::read(uint16_t readAddress, uint8_t* data, uint16_t len){
+void AT24C256::read(uint16_t readAddress, uint8_t* data, uint8_t len){
 
-	Wire.beginTransmission(_address);
-	Wire.write((byte)(readAddress & 0xFF00) >> 8);
-	Wire.write((byte)(readAddress & 0x00FF));
-	Wire.endTransmission();
+	_twi->beginTransmission(_address);
+	_twi->write((byte)(readAddress & 0xFF00) >> 8);
+	_twi->write((byte)(readAddress & 0x00FF));
+	_twi->endTransmission();
 
-	Wire.requestFrom(_address, len);
+	_twi->requestFrom(_address, len);
 	int i;
 	for(i = 0; i < len; i++){
-		if(Wire.available()) data[i] = Wire.read();
+		if(_twi->available()) data[i] = _twi->read();
 	}
 
 }
